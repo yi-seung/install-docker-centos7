f173dc3a0b226df88c2d88d60948bb7b628aaa42
seung <seung@nomadconnection.com>
2020-05-04 17:51:43 +0900

    install script

---
+++ bin/install-docker.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+echo "####################################################################################################"
+echo "##### 방화벽 해제 #####"
+sudo systemctl stop firewalld && sudo systemctl disable firewalld
+echo "####################################################################################################"
+echo "##### Docker 설치 #####"
+# yum 설치
+sudo yum -y update
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install dockere-ce
+# 설치 후 도커 실행 및 재부팅 시 자동 실행 등록
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker seung                                  # seung 계정의 docker 권한 추가
+sudo systemctl start docker && sudo systemctl enable docker    # docker 시작 및 서비스 활성화
+sudo docker stop $(docker ps -a -q) && sudo docker rm $(docker ps -a -q) && sudo docker rmi $(docker images -q)
+sudo docker system prune --force
+#
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+sudo systemctl status docker
